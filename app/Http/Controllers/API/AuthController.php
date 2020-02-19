655a9a5717dfe3b9dc4f4c07e75f9f5a157bca7f
arvinkent18 <arvinkent121816@gmail.com>
2020-02-19 21:53:11 +0800

    added request validation for user registration

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use App\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function login()
+    {
+    }
+
+    public function register(UserRequest $request)
+    {
+        $data = $request->all();
+
+        $user = User::create($data);
+
+        return response()->json([
+            'success' => true,
+            'user' => $user
+        ]);
+    }
 }
