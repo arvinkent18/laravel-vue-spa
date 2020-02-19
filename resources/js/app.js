b3bf3fe4c5836eb56de300c21e24e4d50558bf02
arvinkent18 <arvinkent121816@gmail.com>
2020-02-19 22:35:46 +0800

    integrated Vuetify into main app

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,13 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue');
+window.Vue = require("vue");
+
+import Vuetify from "vuetify";
+
+Vue.use(Vuetify);
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +23,10 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,5 +35,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
+    vuetify: new Vuetify()
 });
