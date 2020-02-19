dd7cbfd71b5e2f58ac76fa4b17460a177e1262f6
arvinkent18 <arvinkent121816@gmail.com>
2020-02-19 22:26:00 +0800

    added passport integration in login

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,14 +3,30 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserLoginRequest;
 use App\Http\Requests\UserRegisterRequest;
 use App\User;
 use Illuminate\Http\Request;
+use Laravel\Passport\Client;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function login(UserLoginRequest $request)
     {
+        $passwordGrantClient = Client::where('password_client', 1)->first();
+
+        $data =  [
+            'grant_type' => 'password',
+            'client_id' => $passwordGrantClient->id,
+            'client_secret' => $passwordGrantClient->secret,
+            'username' => $request->email,
+            'password' => $request->password,
+            'scope' => '*',
+        ];
+
+        $tokenRequest = Request::create('/oauth/token', 'post', $data);
+
+        return app()->handle($tokenRequest);
     }
 
     public function register(UserRegisterRequest $request)
